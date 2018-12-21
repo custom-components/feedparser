7090d4b0599e8d462e3e6e1df9306f526bc17b9a
Joakim Sørensen <joasoe@gmail.com>
2018-12-21 13:36:13 +0100

    Update feedparser.py

--- custom_components/sensor/feedparser.py
+++ custom_components/sensor/feedparser.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import (PLATFORM_SCHEMA)
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['feedparser']
