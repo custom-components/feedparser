b855c5c2ad2eb5a72cb6ae8947519a4254b6fc92
Jenny <pinkywafer@gmail.com>
2019-05-05 14:52:08 +0100

    version bump

--- custom_components/feedparser/sensor.py
+++ custom_components/feedparser/sensor.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['feedparser']
