b491b0fc16cca3ebf531ac8934535825d4c3dfc0
Daniel Apatin <ad@users.noreply.github.com>
2022-04-06 22:19:29 +0300

    Update sensor.py

--- custom_components/feedparser/sensor.py
+++ custom_components/feedparser/sensor.py
@@ -1,96 +1,124 @@
-"""
-A component which allows you to parse an RSS feed into a sensor
+"""Feedparser sensor"""
+from __future__ import annotations
 
-For more details about this component, please refer to the documentation at
-https://github.com/custom-components/sensor.feedparser
-
-Following spec from https://validator.w3.org/feed/docs/rss2.html
-"""
-
+import asyncio
+import re
+from datetime import timedelta
 import logging
 import voluptuous as vol
-from datetime import timedelta
 from dateutil import parser
 from time import strftime
 from subprocess import check_output
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.sensor import SensorEntity
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.sensor import (PLATFORM_SCHEMA)
-
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from fuzzywuzzy import process
 
-__version__ = '0.0.7'
+import feedparser
+
+__version__ = "0.1.0"
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['feedparser', 'fuzzywuzzy']
+COMPONENT_REPO = 'https://github.com/ad/feedparser'
 
-CONF_NAME = 'name'
-CONF_FEEDS_URL = 'feeds_url'
-CONF_LIMIT_PER_FEED = 'limit_per_feed'
-CONF_DATE_FORMAT = 'date_format'
+REQUIREMENTS = ["feedparser", 'fuzzywuzzy']
+
+CONF_FEEDS_URL = "feeds_url"
+CONF_DATE_FORMAT = "date_format"
+CONF_INCLUSIONS = "inclusions"
+CONF_EXCLUSIONS = "exclusions"
+CONF_SHOW_TOPN = "show_topn"
 CONF_STOP_WORDS = 'stop_words'
-CONF_INCLUSIONS = 'inclusions'
-CONF_EXCLUSIONS = 'exclusions'
 
 DEFAULT_SCAN_INTERVAL = timedelta(hours=1)
-
-COMPONENT_REPO = 'https://github.com/ad/feedparser'
-SCAN_INTERVAL = timedelta(minutes=15)
-ICON = 'mdi:rss'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_FEEDS_URL, default=[]): vol.All(cv.ensure_list, [cv.string]),
-    vol.Required(CONF_LIMIT_PER_FEED, default=3): cv.positive_int,
+    vol.Required(CONF_SHOW_TOPN, default=3): cv.positive_int,
     vol.Required(CONF_DATE_FORMAT, default='%a, %b %d %I:%M %p'): cv.string,
     vol.Optional(CONF_STOP_WORDS, default=[]): vol.All(cv.ensure_list, [cv.string]),
     vol.Optional(CONF_INCLUSIONS, default=[]): vol.All(cv.ensure_list, [cv.string]),
     vol.Optional(CONF_EXCLUSIONS, default=[]): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period,
 })
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    add_devices([FeedParserSensor(hass, config)])
+
+@asyncio.coroutine
+def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_devices: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    async_add_devices(
+        [
+            FeedParserSensor(
+                feeds=config[CONF_FEEDS_URL],
+                name=config[CONF_NAME],
+                date_format=config[CONF_DATE_FORMAT],
+                show_topn=config[CONF_SHOW_TOPN],
+                stop_words=config[CONF_STOP_WORDS],
+                inclusions=config[CONF_INCLUSIONS],
+                exclusions=config[CONF_EXCLUSIONS],
+                scan_interval=config[CONF_SCAN_INTERVAL],
+            )
+        ],
+        True,
+    )
+
 
 class FeedParserSensor(SensorEntity):
-    def __init__(self, hass, config):
-        self.hass = hass
-        self._feeds = config[CONF_FEEDS_URL]
-        self._name = config[CONF_NAME]
-        self._limit_per_feed = config[CONF_LIMIT_PER_FEED]
-        self._date_format = config[CONF_DATE_FORMAT]
-        self._stop_words = list(map(lambda x:x.lower(), config[CONF_STOP_WORDS]))
-        self._inclusions = config[CONF_INCLUSIONS]
-        self._exclusions = config[CONF_EXCLUSIONS]
-        self._state = None
-        self.hass.data[self._name] = {}
-        self.update()
+    def __init__(
+        self,
+        feeds: str,
+        name: str,
+        date_format: str,
+        show_topn: str,
+        stop_words: list,
+        exclusions: str,
+        inclusions: str,
+        scan_interval: int,
+    ) -> None:
+        self._feeds = feeds
+        self._attr_name = name
+        self._attr_icon = "mdi:rss"
+        self._date_format = date_format
+        self._show_topn = show_topn
+        self._stop_words = list(map(lambda x:x.lower(), stop_words))
+        self._inclusions = inclusions
+        self._exclusions = exclusions
+        self._scan_interval = scan_interval
+        self._attr_state = None
+        self._entries = []
+        self._attr_extra_state_attributes = {"entries": self._entries}
 
     def update(self):
-        import feedparser
-
-        self.hass.data[self._name] = {}
+        self._entries = []
 
         for feed in self._feeds:
             count = 0
-            parsedFeed = feedparser.parse(feed)
 
-            if not parsedFeed:
+            parsed_feed = feedparser.parse(feed)
+            if not parsed_feed:
                 continue
             else:
-                for entry in parsedFeed.entries:
+                for entry in parsed_feed.entries:
                     title = entry['title'] if entry['title'] else entry['description']
 
-                    if not title or title in self.hass.data[self._name]:
+                    if not title or title in self._entries:
                         continue
                     
-                    if len(self.hass.data[self._name]) > 0:
-                        (highest_title, highest) = process.extractOne(title, [*self.hass.data[self._name].keys()])
+                    if len(self._entries) > 0:
+                        (highest_title, highest) = process.extractOne(title, [*self._entries.keys()])
                         if highest > 75:
                             #_LOGGER.error("%s is very similar to: %s" % (title, highest_title))
                             continue
 
-                    if count < self._limit_per_feed:
+                    if count < self._show_topn:
                         skip_title = False
                         if self._stop_words:
                             lower_title = title.lower()
@@ -101,7 +129,7 @@ class FeedParserSensor(SensorEntity):
                                     continue
                         
                         if skip_title != True:    
-                            self.hass.data[self._name][title] = {}
+                            self._entries[title] = {}
 
                             for key, value in entry.items():
                                 if (self._inclusions and key not in self._inclusions) or ('parsed' in key) or (key in self._exclusions):
@@ -110,23 +138,19 @@ class FeedParserSensor(SensorEntity):
                                 if key in ['published', 'updated', 'created', 'expired']:
                                     value = parser.parse(value).strftime(self._date_format)
 
-                                self.hass.data[self._name][title][key] = value
+                                self._entries[title][key] = value
                             count += 1
 
-        self._state = len(self.hass.data[self._name])
-
-    @property
-    def name(self):
-        return self._name
+                        self._entries.append(entry_value) 
+        self._attr_state = (
+            len(self._entries)
+        )
 
     @property
     def state(self):
-        return self._state
+        """Return the state of the sensor."""
+        return self._attr_state
 
     @property
-    def icon(self):
-        return ICON
-
-    @property
-    def device_state_attributes(self):
-        return self.hass.data[self._name]
+    def extra_state_attributes(self):
+        return {"entries": self._entries}
