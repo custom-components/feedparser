51500df26082cb32bb9d191a5086aef3b80335d7
Ondřej Gajdušek <ogajduse@redhat.com>
2023-06-28 23:01:15 +0200

    Release 0.1.8

--- custom_components/feedparser/sensor.py
+++ custom_components/feedparser/sensor.py
@@ -17,7 +17,7 @@ import homeassistant.util.dt as dt
 
 import feedparser
 
-__version__ = "0.1.6"
+__version__ = "0.1.8"
 
 COMPONENT_REPO = "https://github.com/custom-components/sensor.feedparser/"
 
